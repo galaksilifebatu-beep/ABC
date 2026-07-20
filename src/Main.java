a5e4a8e4a9a127dfaa0e28094b2c0fb8cf693b28
galaksilifebatu-beep <galaksilifebatu@gmail.com>
2026-07-20 15:36:24 +0300

    example1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("batu");
-        }
 
+        //HashMap ile HashSet farklılıkları göstermek için örnek bir kod yazalım.
         HashMap<String, Integer> map = new HashMap<>();
         map.put("one", 1);
         map.put("two", 2);
@@ -30,6 +26,59 @@ public class Main {
         for (Map.Entry<String, Integer> item : map.entrySet()) {
             System.out.println(item.getKey() + ": " + item.getValue());
         }
+
+        List<Integer> listarr = new ArrayList<>();
+        List<Integer> numbers = new LinkedList<>();
+
+        double durationAddArrayList = timer((v) -> {
+            listarr.add(1);
+            return null;
+        });
+
+        double durationAddLinkedList = timer((v) -> {
+            numbers.add(1);
+            return null;
+        });
+
+        System.out.println("arrayList ile linkedList Fark: " + (durationAddArrayList - durationAddLinkedList) + " ms");
+
+        for (int i = 2; i < 100; i++) {
+            listarr.add(i);
+            numbers.add(i);
+        }
+
+        double durationGetArrayList = timer((v) -> {
+            listarr.get(30);
+            return null;
+        });
+
+        double durationGetLinkedList = timer((v) -> {
+            numbers.get(30);
+            return null;
+        });
+
+        System.out.println("arrayList ile linkedList Fark: " + (durationGetArrayList - durationGetLinkedList) + " ms");
+       
+
+    }
+
+    private static double timer(Function<Void, Object> function) {
+        //ArrayList ve LinkedList farklılıklarını göstermek için örnek bir kod yazalım.
+        
+        long startTime = System.nanoTime();
+        
+        for (int i = 0; i < 1000000; i++) {
+            Math.sqrt(i);
+            function.apply(null);
+            
+        }
+        
+        long endTime = System.nanoTime();
+        long durationInNanoseconds = endTime - startTime;
+        
+        
+        double durationInMilliseconds = durationInNanoseconds / 1000000.0;
+        return durationInMilliseconds;
     }
 }
 
