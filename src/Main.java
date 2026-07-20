4d0075b21b3803f3b00e89626148f9dd76a5d2c1
galaksilifebatu-beep <galaksilifebatu@gmail.com>
2026-07-20 15:10:40 +0300

    java25settings_optimized

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,36 @@
 
-void main() {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
 
-    for (int i = 1; i <= 5; i++) {
-        System.out.println("batu");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("batu");
+        }
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+
+        for (Integer item : set) {
+            System.out.println(item);
+        }
+
+        for (Map.Entry<String, Integer> item : map.entrySet()) {
+            System.out.println(item.getKey() + ": " + item.getValue());
+        }
     }
-
-    HashMap<String, String> map = new HashMap<>();
-    map.put("one", "batu1");
-    map.put("two", "batu2");
-    map.put("three", "batu3");
-
-    for (Map.Entry<String, String> entry : map.entrySet()) {
-        System.out.println(entry.getKey() + ": " + entry.getValue());
-    }
-
-
 }
+
+
