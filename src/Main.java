352b3131761d894c122bd4260299a8668ab9f560
galaksilifebatu-beep <galaksilifebatu@gmail.com>
2026-07-20 12:51:27 +0300

    abi

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+
 
     for (int i = 1; i <= 5; i++) {
         System.out.println("batu");
     }
+
+    HashMap<String, String> map = new HashMap<>();
+    map.put("one", "batu1");
+    map.put("two", "batu2");
+    map.put("three", "batu3");
+
+    for (Map.Entry<String, String> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + ": " + entry.getValue());
+    }
+
+
 }
